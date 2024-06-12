b58969ff34be9c88e43a290ae77985f8dec63195
Leonardo de Freitas dos Santos Silva <146646494+leonardo-feitas-dev@users.noreply.github.com>
2024-06-12 15:27:14 -0300

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ def get_db_connection():
     conn = psycopg2.connect(
             user="avnadmin",
             password="I67KnpDQtqo7Zw0kz6fXvJFZsm4fLCK1",
-            host="dpg-cpkuddnsc6pc73f50vl0-a",
+            host="dpg-cpkuddnsc6pc73f50vl0-a.oregon-postgres.render.com",
             port="5432",
             database="dbia"
             )
