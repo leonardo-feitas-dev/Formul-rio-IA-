1c0d8377b1c8ff907cfe6d54a737a45d8a449a44
Leonardo de Freitas dos Santos Silva <146646494+leonardo-feitas-dev@users.noreply.github.com>
2024-06-12 14:47:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ def modelos():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
